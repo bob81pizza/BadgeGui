bd307d464e902e95494e3e9906e00d87bf8b35dd
cwm24 <cwm24@ITARGET1096.univ.pitt.edu>
2015-02-17 11:04:30 -0500

    Initialized Repo

---
+++ src/badgegui/BadgeGui.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package badgegui;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author cwm24
+ */
+public class BadgeGui {
+
+    private final MainFrame frame = new MainFrame();
+    private Parameters p;
+    Map<String, Color> colors = new HashMap();
+    Map<String, Integer> fonts = new HashMap();
+    Color color;
+    Color bgColor;
+    /**
+     * @param args the command line arguments
+     */
+       
+    public BadgeGui(){
+        colors.put("blue", Color.blue);
+        colors.put("black", Color.black);
+        colors.put("cyan", Color.cyan);
+        colors.put("darkGray", Color.darkGray);
+        colors.put("gray", Color.gray);
+        colors.put("green", Color.green);
+        colors.put("lightGray", Color.lightGray);
+        colors.put("magenta", Color.magenta);
+        colors.put("orange", Color.orange);
+        colors.put("pink", Color.pink);
+        colors.put("red", Color.red);
+        colors.put("white", Color.white);
+        colors.put("yellow", Color.yellow);
+
+        fonts.put("REGULAR", Font.PLAIN);
+        fonts.put("BOLD", Font.BOLD);
+        fonts.put("ITALIC", Font.ITALIC);
+               
+        frame.addColorActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                draw();
+            }
+        });
+        
+        frame.addBgColorActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                draw();
+            }
+        });
+
+    }
+
+    public void draw(){
+        System.out.println(frame.getShapeColor());
+        color = colors.get(frame.getShapeColor());
+        bgColor = colors.get(frame.getBgColor());
+        frame.draw(color, bgColor);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BadgeGui app = new BadgeGui();
+        app.frame.setVisible(true);
+    }
+    
+}
---
+++ src/badgegui/Parameters.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package badgegui;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author cwm24
+ */
+public class Parameters {
+    private String name;
+    private String shape;
+    private Color color;
+    private Color bgcolor;
+    private String stringX;
+    private String stringY;
+    private String ftstyle;
+    private String ftsizeString;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the shape
+     */
+    public String getShape() {
+        return shape;
+    }
+
+    /**
+     * @param shape the shape to set
+     */
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the bgcolor
+     */
+    public Color getBgcolor() {
+        return bgcolor;
+    }
+
+    /**
+     * @param bgcolor the bgcolor to set
+     */
+    public void setBgcolor(Color bgcolor) {
+        this.bgcolor = bgcolor;
+    }
+
+    /**
+     * @return the stringX
+     */
+    public String getStringX() {
+        return stringX;
+    }
+
+    /**
+     * @param stringX the stringX to set
+     */
+    public void setStringX(String stringX) {
+        this.stringX = stringX;
+    }
+
+    /**
+     * @return the stringY
+     */
+    public String getStringY() {
+        return stringY;
+    }
+
+    /**
+     * @param stringY the stringY to set
+     */
+    public void setStringY(String stringY) {
+        this.stringY = stringY;
+    }
+
+    /**
+     * @return the ftstyle
+     */
+    public String getFtstyle() {
+        return ftstyle;
+    }
+
+    /**
+     * @param ftstyle the ftstyle to set
+     */
+    public void setFtstyle(String ftstyle) {
+        this.ftstyle = ftstyle;
+    }
+
+    /**
+     * @return the ftsizeString
+     */
+    public String getFtsizeString() {
+        return ftsizeString;
+    }
+
+    /**
+     * @param ftsizeString the ftsizeString to set
+     */
+    public void setFtsizeString(String ftsizeString) {
+        this.ftsizeString = ftsizeString;
+    }
+}
